b6a3385bab9e194e5eef6f4acef2145b1123f290
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-18 22:21:20 +0300

    Adding new migration RoleIntegerRoleId

--- TestTaskApi/src/TestTaskApi/Migrations/20210718185233_RoleIntegerRoleId.Designer.cs
+++ TestTaskApi/src/TestTaskApi/Migrations/20210718185233_RoleIntegerRoleId.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TestTaskApi.Data;
 
 namespace TestTaskApi.Migrations
 {
     [DbContext(typeof(TestTaskDbContext))]
-    partial class TestTaskDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210718185233_RoleIntegerRoleId")]
+    partial class RoleIntegerRoleId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TestTaskApi/src/TestTaskApi/Migrations/20210718185233_RoleIntegerRoleId.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestTaskApi.Migrations
+{
+    public partial class RoleIntegerRoleId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Account_Role_RoleId",
+                table: "Account");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Role",
+                table: "Role");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Account_RoleId",
+                table: "Account");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Role_RoleName",
+                table: "Role");
+
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "Account");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Role");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Role",
+                type: "int",
+                nullable: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoleId",
+                table: "Account",
+                type: "int",
+                nullable: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Role_RoleName",
+                table: "Role",
+                column: "RoleName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Role_RoleName",
+                table: "Role");
+
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "Account");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Role");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Id",
+                table: "Role",
+                type: "nvarchar(450)",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoleId",
+                table: "Account",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Role_RoleName",
+                table: "Role",
+                column: "RoleName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_RoleId",
+                table: "Account",
+                column: "RoleId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Role",
+                table: "Role",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Account_Role_RoleId",
+                table: "Account",
+                column: "RoleId",
+                principalTable: "Role",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
