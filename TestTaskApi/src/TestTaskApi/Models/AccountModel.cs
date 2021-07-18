509699be0c3cd56a0c6a099eed72b0f11870ff69
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-18 18:21:31 +0300

    Adding some models: AccountModel, RoleModel

---
+++ TestTaskApi/src/TestTaskApi/Models/AccountModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TestTaskApi.Models
+{
+    public class AccountModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public RoleModel Role { get; set; }
+    }
+}
---
+++ TestTaskApi/src/TestTaskApi/Models/RoleModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TestTaskApi.Models
+{
+    public class RoleModel
+    {
+        public string Id { get; set; }
+        public string RoleName { get; set; }
+    }
+}
