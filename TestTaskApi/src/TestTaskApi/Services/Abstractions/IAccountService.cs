ee72151bb58f01cc6c46a1082134ed51d4fa1315
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-18 18:23:02 +0300

    Adding interface IAccountService

---
+++ TestTaskApi/src/TestTaskApi/Services/Abstractions/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using TestTaskApi.Models.Responses;
+
+namespace TestTaskApi.Services.Abstractions
+{
+    public interface IAccountService
+    {
+        Task AddRole(string role);
+        Task Registration(string email, string password, string roleName);
+        Task<AuthentificationResponse> Authentification(string email, string password);
+    }
+}
