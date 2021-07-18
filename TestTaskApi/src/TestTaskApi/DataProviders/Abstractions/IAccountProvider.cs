52db686a2a124715035eb5d8f441a0c87be3bc23
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-18 18:11:00 +0300

    Adding interface IAccountProvider

---
+++ TestTaskApi/src/TestTaskApi/DataProviders/Abstractions/IAccountProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using TestTaskApi.Data.Entities;
+
+namespace TestTaskApi.DataProviders.Abstractions
+{
+    public interface IAccountProvider
+    {
+        Task AddRole(string role);
+        Task Registration(string email, string password, string roleName);
+        Task<AccountEntity> Authentification(string email, string password);
+    }
+}
