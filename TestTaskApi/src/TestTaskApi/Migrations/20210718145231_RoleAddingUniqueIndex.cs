27e074f59263e6d4a648b5b6d0b4192f49625613
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-18 18:05:44 +0300

    Adding migration RoleAddingUniqueIndex

---
+++ TestTaskApi/src/TestTaskApi/Migrations/20210718145231_RoleAddingUniqueIndex.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestTaskApi.Migrations
+{
+    public partial class RoleAddingUniqueIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Role_RoleName",
+                table: "Role",
+                column: "RoleName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Role_RoleName",
+                table: "Role");
+        }
+    }
+}
