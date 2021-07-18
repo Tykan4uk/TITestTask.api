160317fb1cc01c2a4aa909eef55d103449cabdb6
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-19 00:50:33 +0300

    Adding interface IMessageProvider

---
+++ TestTaskApi/src/TestTaskApi/DataProviders/Abstractions/IMessageProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TestTaskApi.Data.Entities;
+
+namespace TestTaskApi.DataProviders.Abstractions
+{
+    public interface IMessageProvider
+    {
+        Task<List<MessageEntity>> GetAsync(string accountId);
+        Task<string> AddAsync(string accountId, string message);
+        Task<bool> UpdateAsync(string messageId, string message);
+        Task<bool> RemoveAsync(string messageId);
+    }
+}
