1f1dd872b3acb1dc5f69969f5e23b106a7ed90d8
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-18 18:27:14 +0300

    Add some requests: AddRoleRequest, RegistrationRequest, LoginRequest

---
+++ TestTaskApi/src/TestTaskApi/Models/Requests/AddRoleRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestTaskApi.Models.Requests
+{
+    public class AddRoleRequest
+    {
+        [Required]
+        [StringLength(50)]
+        public string Role { get; set; }
+    }
+}
---
+++ TestTaskApi/src/TestTaskApi/Models/Requests/RegistrationRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestTaskApi.Models.Requests
+{
+    public class RegistrationRequest
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
