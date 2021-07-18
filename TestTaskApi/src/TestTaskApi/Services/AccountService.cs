9727e19ae15295b082b56dc896f1bc4b99c57dab
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-18 18:23:11 +0300

    Adding class AccountService

---
+++ TestTaskApi/src/TestTaskApi/Services/AccountService.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using TestTaskApi.DataProviders.Abstractions;
+using TestTaskApi.Models;
+using TestTaskApi.Models.Responses;
+using TestTaskApi.Services.Abstractions;
+
+namespace TestTaskApi.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountProvider _accountProvider;
+        private readonly IMapper _mapper;
+
+        public AccountService(
+            IAccountProvider accountProvider,
+            IMapper mapper)
+        {
+            _accountProvider = accountProvider;
+            _mapper = mapper;
+        }
+
+        public async Task AddRole(string role)
+        {
+            await _accountProvider.AddRole(role);
+        }
+
+        public async Task Registration(string email, string password, string roleName)
+        {
+            await _accountProvider.Registration(email, password, roleName);
+        }
+
+        public async Task<AuthentificationResponse> Authentification(string email, string password)
+        {
+            var account = await _accountProvider.Authentification(email, password);
+            return new AuthentificationResponse() { Account = _mapper.Map<AccountModel>(account) };
+        }
+    }
+}
