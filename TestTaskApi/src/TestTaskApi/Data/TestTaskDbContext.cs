7f9276351d1e851503a8b6fbbb11b4b012ac0c16
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-18 14:23:07 +0300

    Adding class TestTaskDbContext

---
+++ TestTaskApi/src/TestTaskApi/Data/TestTaskDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTaskApi.Data.Entities;
+using TestTaskApi.Data.EntityConfigurations;
+
+namespace TestTaskApi.Data
+{
+    public class TestTaskDbContext : DbContext
+    {
+        public TestTaskDbContext(DbContextOptions<TestTaskDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AccountEntity> Accounts { get; set; }
+        public DbSet<RoleEntity> Roles { get; set; }
+        public DbSet<MessageEntity> Messages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AccountEntityConfiguration());
+            modelBuilder.ApplyConfiguration(new RoleEntityConfiguration());
+            modelBuilder.ApplyConfiguration(new MessageEntityConfiguration());
+        }
+    }
+}
