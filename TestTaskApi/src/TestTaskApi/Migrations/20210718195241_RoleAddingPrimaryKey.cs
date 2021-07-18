e166cbb7e9696b63ec6db63b534f34f713f754da
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-18 23:26:25 +0300

    Adding migration RoleAddingPrimaryKey

---
+++ TestTaskApi/src/TestTaskApi/Migrations/20210718195241_RoleAddingPrimaryKey.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestTaskApi.Migrations
+{
+    public partial class RoleAddingPrimaryKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Role",
+                table: "Role",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Role",
+                table: "Role");
+        }
+    }
+}
