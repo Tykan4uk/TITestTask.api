6079ce26356f4b6fab0bc925000eb812e0b76bba
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-19 00:52:07 +0300

    Adding MessageService

---
+++ TestTaskApi/src/TestTaskApi/Services/MessageService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using TestTaskApi.DataProviders.Abstractions;
+using TestTaskApi.Models;
+using TestTaskApi.Models.Responses;
+using TestTaskApi.Services.Abstractions;
+
+namespace TestTaskApi.Services
+{
+    public class MessageService : IMessageService
+    {
+        private readonly IMessageProvider _messageProvider;
+        private readonly IMapper _mapper;
+
+        public MessageService(
+            IMessageProvider messageProvider,
+            IMapper mapper)
+        {
+            _messageProvider = messageProvider;
+            _mapper = mapper;
+        }
+
+        public async Task<GetResponse> GetAsync(string accountId)
+        {
+            var result = await _messageProvider.GetAsync(accountId);
+            return new GetResponse() { Messages = _mapper.Map<List<MessageModel>>(result) };
+        }
+
+        public async Task<AddResponse> AddAsync(string accountId, string message)
+        {
+            var result = await _messageProvider.AddAsync(accountId, message);
+            return new AddResponse() { MessageId = result };
+        }
+
+        public async Task<UpdateResponse> UpdateAsync(string messageId, string message)
+        {
+            var result = await _messageProvider.UpdateAsync(messageId, message);
+            return new UpdateResponse() { IsUpdated = result };
+        }
+
+        public async Task<RemoveResponse> RemoveAsync(string messageId)
+        {
+            var result = await _messageProvider.RemoveAsync(messageId);
+            return new RemoveResponse() { IsDeleted = result };
+        }
+    }
+}
