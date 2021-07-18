f0e13ba7fd09705c40010852df8ee56c307dac70
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-18 14:23:52 +0300

    Startup: registration TestTaskDbContext

--- TestTaskApi/src/TestTaskApi/Startup.cs
+++ TestTaskApi/src/TestTaskApi/Startup.cs
@@ -1,20 +1,27 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using TestTaskApi.Configurations;
+using TestTaskApi.Data;
 
 namespace TestTaskApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup()
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            AppConfiguration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration AppConfiguration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -23,6 +30,10 @@ namespace TestTaskApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TestTaskApi", Version = "v1" });
             });
+            services.Configure<Config>(AppConfiguration);
+            var connectionString = AppConfiguration["TestTaskApi:ConnectionString"];
+            services.AddDbContext<TestTaskDbContext>(
+                opts => opts.UseSqlServer(connectionString));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
