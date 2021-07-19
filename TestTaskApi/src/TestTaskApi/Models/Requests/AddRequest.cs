55efb0937211a815be30d00d0936da3d267f2f32
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-19 14:37:05 +0300

    AddRequest and UpdateRequest: adding max length of message 255 (as in DB)

--- TestTaskApi/src/TestTaskApi/Models/Requests/AddRequest.cs
+++ TestTaskApi/src/TestTaskApi/Models/Requests/AddRequest.cs
@@ -1,8 +1,11 @@
-﻿namespace TestTaskApi.Models.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestTaskApi.Models.Requests
 {
     public class AddRequest
     {
         public string AccountId { get; set; }
+        [StringLength(255)]
         public string Message { get; set; }
     }
 }
--- TestTaskApi/src/TestTaskApi/Models/Requests/UpdateRequest.cs
+++ TestTaskApi/src/TestTaskApi/Models/Requests/UpdateRequest.cs
@@ -1,9 +1,12 @@
-﻿namespace TestTaskApi.Models.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestTaskApi.Models.Requests
 {
     public class UpdateRequest
     {
         public string AccountId { get; set; }
         public string MessageId { get; set; }
+        [StringLength(255)]
         public string Message { get; set; }
     }
 }
