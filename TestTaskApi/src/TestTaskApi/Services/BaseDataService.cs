5c64d8cb80f7b771e7975224c27458b5de3378bf
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-19 23:28:28 +0300

    Adding class-wrap BaseDataService

---
+++ TestTaskApi/src/TestTaskApi/Services/BaseDataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestTaskApi.Data;
+
+namespace TestTaskApi.Services
+{
+    public class BaseDataService
+    {
+        private readonly TestTaskDbContext _testTaskDbContext;
+
+        public BaseDataService(IDbContextFactory<TestTaskDbContext> dbContextFactory)
+        {
+            _testTaskDbContext = dbContextFactory.CreateDbContext();
+        }
+
+        protected async Task<T> ExecuteSafe<T>(Func<Task<T>> action)
+        {
+            using (var transaction = _testTaskDbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    var result = await action();
+                    transaction.Commit();
+                    return result;
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+    }
+}
